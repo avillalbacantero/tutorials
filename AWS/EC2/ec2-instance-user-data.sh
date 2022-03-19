fb9dc597c5e8dc50c487719ebc4d48b3ec50fdf4
avillalbacantero <55324761+avillalbacantero@users.noreply.github.com>
2022-03-19 20:08:43 +0100

    Delete AWS/EC2 directory

--- AWS/EC2/ec2-instance-user-data.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-yum update -y
-yum install httpd -y
-systemctl start httpd
-systemctl enable httpd
-cd /var/www/html
-echo "This is a test page running on Apache on ECS in the AWS Cloud" > index.html
