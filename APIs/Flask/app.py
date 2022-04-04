e7250afc1a177d4da6c1516c61c6631cc2d25161
avillalbacantero <villalbacanteroa@gmail.com>
2022-04-04 18:22:24 +0200

    Add HTML Templates & simple CSS

--- APIs/Flask/app.py
+++ APIs/Flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # Setup the app
 app = Flask(__name__)
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # Create a index route for browing the URL
 @app.route('/')
 def index():
-    return "Hello, world!"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
