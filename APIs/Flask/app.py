b22d43b8c2f4e493a4e0f5055c187dc162856649
avillalbacantero <villalbacanteroa@gmail.com>
2022-04-04 18:36:15 +0200

    Add table and SQLAlchemy

--- APIs/Flask/app.py
+++ APIs/Flask/app.py
@@ -1,7 +1,22 @@
+from datetime import datetime
+
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 # Setup the app
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///test.db'
+db = SQLAlchemy(app)  # initialize the DB
+
+#db.create_all()  # create DB
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    def __repr__(self) -> str:
+        return f"<Task {self.id}>"
 
 # Create a index route for browing the URL
 @app.route('/')
